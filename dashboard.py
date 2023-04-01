c4699abb92af7547b639288c1d7ea49f2e0785c1
Elena1505 <eb.lelou@gmail.com>
2023-04-01 16:23:24 +0200

    Started the dashboard with streamlit

---
+++ dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def main():
+    st.title('Interactive dashboard')
+    st.markdown("Welcome to this Interactive Dashboard! In a first part, you will find general information"
+                " about customers and features importances. In a second part you will find more specific"
+                " informations about specific customer. ")
+
+
+if __name__ == '__main__':
+    main()
